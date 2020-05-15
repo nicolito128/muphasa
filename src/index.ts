f2f1516175315b895db9df670bcd6e31932dbfdd
Nicolás Serna <nicolito128@hotmail.com>
2020-05-15 15:18:47 -0300

    Add loadPlugins to start the bot

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ global.Config = Config
 import { Client } from "./client"
 global.Client = Client
 
+import { Plugins } from "./plugins"
+global.Plugins = Plugins
+
+// Load plugins
+Plugins.loadPlugins()
+
 Client.connect()
