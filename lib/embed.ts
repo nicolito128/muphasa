b6a32a471bcb49b77afcd94146c632042e9a3ded
Nicolás Serna <nicolito128@hotmail.com>
2020-05-15 18:23:53 -0300

    Add embed.ts

---
+++ lib/embed.ts
@@ -0,0 +1,42 @@
+'use strict';
+import * as Discord from "discord.js"
+
+export interface IEmbed {
+    options: IEmbedOptions;
+
+    create(options?: IEmbedOptions): CustomEmbed;
+    denied(): CustomEmbed;
+    notify(title: string, desc: string | string[], color: Discord.ColorResolvable): CustomEmbed;
+}
+
+interface IEmbedOptions {
+    [o: string]: any;
+}
+
+class CustomEmbed extends Discord.MessageEmbed implements IEmbed {
+    options: IEmbedOptions;
+
+    constructor(options: IEmbedOptions) {
+        super(options)
+    }
+
+    create(options?: IEmbedOptions): CustomEmbed {
+        return new CustomEmbed(options || {})
+    }
+
+    denied(): CustomEmbed {
+        return this.create()
+            .setTitle('Acceso denegado')
+            .setColor([213, 41, 32])
+            .setDescription(`No tienes suficiente autoridad para usar este comando.`)
+    }
+
+    notify(title: string, desc: string | string[], color: Discord.ColorResolvable = [57, 140, 232]): CustomEmbed {
+        return this.create()
+            .setTitle(title)
+            .setColor(color)
+            .setDescription(desc)
+    }
+}
+
+export const Embed: CustomEmbed = new CustomEmbed({});
