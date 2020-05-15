626d956863e1a2c171e7be64ceaccf16c5e24ae2
Nicolás Serna <nicolito128@hotmail.com>
2020-05-15 18:23:18 -0300

    Update global

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+/**
+ * Main File
+ * 
+ * This is the main Bot app, and the file you should be
+ * running to start the bot if you're using it normally.
+ * 
+*/
 'use strict';
+// Check for version
+try {
+	RegExp("\\p{Emoji}", "u");
+} catch (e) {
+	throw new Error("We require Node.js version 10 or later; you're using " + process.version);
+}
+
+/**********************
+ * Set globals
+ **********************/
+declare const global: any
 
 import * as Config from "./../config/config.js"
 global.Config = Config
@@ -10,6 +28,10 @@ import { Plugins } from "./plugins"
 global.Plugins = Plugins
 
 // Load plugins
-Plugins.loadPlugins()
+try {
+    Plugins.loadPlugins()
+} catch (err) {
+    if (err) throw new Error(err)
+}
 
 Client.connect()
