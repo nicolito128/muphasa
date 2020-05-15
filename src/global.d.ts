eb8d03dba0bf9c9c37336d16f9bb1c5f8f0c19b5
Nicolás Serna <nicolito128@hotmail.com>
2020-05-15 15:19:33 -0300

    Add global Plugins and types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,20 @@
 import { IClient } from "./client"
+import * as Plugins from "./plugins"
 
 declare global {
     namespace NodeJS {
         interface Global {
             Config: {[k: string]: any};
             Client: IClient;
+            Plugins: Plugins.IPlugins;
         }
     }
+
+    // Global types
+    namespace Types {
+        type ICommands = Plugins.ICommands
+        type IHelps = Plugins.IHelps;
+        type Message = Plugins.Message
+    }
 }
+
