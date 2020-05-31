6f43e14887bd29d3aa786c03ad340fc18b4a6d75
Nicolás Serna <nicolito128@hotmail.com>
2020-05-31 18:42:44 -0300

    Update embed.ts

--- lib/embed.ts
+++ lib/embed.ts
@@ -5,6 +5,19 @@ interface IEmbedOptions {
     [o: string]: any;
 }
 
+function generateRandomHex(): string {
+    const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+    let random: number
+    let hex: string = '#';
+
+    for (let i = 0, l = hexNumbers.length; i < 6; i++) {
+        random = Math.round(Math.random() * (l - 1))
+        hex += hexNumbers[random]
+    }
+
+    return hex
+}
+
 export class CustomEmbed extends Discord.MessageEmbed {
     constructor(options: IEmbedOptions) {
         super(options)
@@ -22,7 +35,7 @@ export class CustomEmbed extends Discord.MessageEmbed {
     }
 
     notify(title: string, desc: string | string[], color?: Discord.ColorResolvable): CustomEmbed {
-        if (!color) color = [57, 140, 232]
+        if (!color) color = generateRandomHex()
         return this.create()
             .setTitle(title)
             .setColor(color)
