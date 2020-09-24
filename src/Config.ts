4b9295203ceb9fdf782282a207aed7cabcdbf0f1
Nicolás Serna <nicolito128@hotmail.com>
2020-09-23 21:16:45 -0300

    Update Config

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export default class Config {
 	static readonly token = process.env.BOT_TOKEN || "";
 	static readonly owner = process.env.BOT_OWNER || "";
-	static readonly prefix = 'mu.';
+	static readonly prefix = 'mu';
 };
