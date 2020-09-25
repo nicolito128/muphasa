89edf0269df837c738e1b5e3b809c73e56e5180a
Nicolás Serna <nicolito128@hotmail.com>
2020-09-24 21:17:39 -0300

    Update eval command

--- src/commands/admin/eval.ts
+++ src/commands/admin/eval.ts
@@ -17,14 +17,14 @@ export = class EvalCommand extends Command {
         const code: string = targets.join(' ')
         if (!code) {
         	message.channel.send('Ingresa código que poder evaluar.')
-        	return
+        	return;
         }
 
         try {
             message.channel.send(eval(code), {code: 'javascript'})
-            console.log('\nEVAL OUTPUT:\n' + eval(code))
         } catch(err) {
-            message.channel.send(`ERROR!\n${err}`, {code: 'javascript'})
+            message.channel.send(`¡Ha sucedido un error!`)
+            message.channel.send(err, {code: 'javascript'})
         }
 	}
 }
