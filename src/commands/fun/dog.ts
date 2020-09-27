41d35ec18af24b179c2c6063e964eea055bdc006
Nicolás Serna <nicolito128@hotmail.com>
2020-09-27 15:29:16 -0300

    Add dog command

---
+++ src/commands/fun/dog.ts
@@ -0,0 +1,42 @@
+import * as superagent from 'superagent'
+import { Command, RunArguments } from './../../lib/command'
+import { Embed } from './../../lib/embed'
+
+interface DogResponse {
+    message: string;
+    status: string;
+}
+
+export = class CatCommand extends Command {
+    readonly apiURL = 'https://dog.ceo/api/breeds/image/random'
+
+	constructor(){
+		super({
+			name: 'dog',
+			desc: '¡Devuelve perritos!',
+			group: 'fun',
+            alias: ['goodboy']
+		})
+	}
+
+	async run({message, user}: RunArguments) {
+        try{
+            const image = await this.getRandomDog()
+            const embed = Embed.notify({title: 'Dog!', desc: ''})
+            .setURL(image.message)
+            .setImage(image.message)
+            .setAuthor(user.tag, user.displayAvatarURL())
+            .setFooter('Impulsado por dog.ceo', 'https://dog.ceo/img/dog-api-logo.svg')
+
+            message.channel.send(embed)
+        } catch(error) {
+            message.channel.send('Ha ocurrido un error al consultar la api...')
+            console.log(error)
+        }
+	}
+
+    async getRandomDog(): Promise<DogResponse> {
+        const cat = await superagent.get(this.apiURL)
+        return cat.body as unknown as DogResponse
+    }
+}
