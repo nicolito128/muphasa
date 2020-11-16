7aed0cd9b3240a97b7c8186afbd5b986591e3648
Nicolás Serna <nicolito128@hotmail.com>
2020-11-16 18:45:06 -0300

    Add slap command

---
+++ src/commands/actions/slap.ts
@@ -0,0 +1,56 @@
+import { User } from 'discord.js'
+import { Command, RunArguments } from './../../lib/command'
+import { Embed } from './../../lib/embed'
+import Searcher from './../../lib/giphy'
+
+export = class HugCommand extends Command {
+	readonly slaps: Searcher;
+	readonly phrases: string[];
+
+	constructor() {
+		super({
+			name: 'slap',
+			desc: 'Abofetea a un usuario.',
+			usage: '@mention',
+			group: 'actions',
+			cooldown: 10
+		})
+
+		this.slaps = new Searcher({
+			search: 'slap'
+		})
+
+		this.phrases = [
+			'¡**$1** abofetea a **$0**!'
+		]
+	}
+
+	async run({message, client, user}: RunArguments) {
+		if (!message.mentions.users.first()) {
+			message.channel.send('Necesito que menciones a un usuario.')
+			return;
+		}
+
+		if (message.mentions.users.first()?.id === client.user?.id) {
+			message.channel.send('No pienso hacer eso.')
+			return;
+		}
+
+		const targetUser = message.mentions.users.first() as User;
+		const embed = Embed.notify({
+			title: this.getRandomPhrase(targetUser.username, user.username),
+			desc: ''
+		})
+		.setFooter('Impulsado por Giphy API', 'https://i.imgur.com/gLB2xaj.png')
+
+		await this.hugs.getResponse().then(img => embed.setImage(img?.data.image_url || ""))
+
+		message.channel.send(embed)
+	}
+
+	getRandomPhrase(first: string, second: string): string {
+		const len = this.phrases.length;
+		const random = Math.round(Math.random() * len)
+		return this.phrases[random].replace(/\$0/g, first).replace(/\$1/g, second)
+	}
+}
