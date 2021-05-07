85fd85037c453585f79ae2459abb19f10504201a
Nicolás Abel Serna <nicolito128@hotmail.com>
2021-05-07 15:34:07 -0300

    Update invite command

--- src/commands/info/invite.ts
+++ src/commands/info/invite.ts
@@ -2,12 +2,16 @@ import { CommandContext, Arguments } from './../../lib/command'
 import { Embed } from './../../lib/embed'
 
 export = class InviteCommand extends CommandContext {
+    readonly inviteURL: string;
+
 	constructor(){
 		super({
 			name: 'invite',
 			desc: 'Proporciona enlaces útiles para invitarme a tu servidor.',
 			group: 'info'
 		})
+
+        this.inviteURL = 'https://discord.com/oauth2/authorize?client_id=551826544453222418&scope=bot&permissions=1543896183'
 	}
 
 	run({message, client}: Arguments) {
@@ -15,7 +19,7 @@ export = class InviteCommand extends CommandContext {
         .setDescription([
             '¡Hola! soy **' + client.user?.username + '**, un bot multi propocitos con diversas herramienta para mejorar tu servidor.',
             ' ',
-            `:ok_hand: **[Invitación con permisos](https://discord.com/oauth2/authorize?client_id=551826544453222418&scope=bot&permissions=8)**`,
+            `:ok_hand: **[Invitación con permisos](${this.inviteURL})**`,
             `:pinching_hand: [Invitación sin permisos](https://discord.com/oauth2/authorize?client_id=551826544453222418&scope=bot&permissions=0)`
         ])
         .setColor('#4169E1')
