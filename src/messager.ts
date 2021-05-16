9e72aaf29d74c22d1d3ff626c8bc8c942b994f31
Nicolás Abel Serna <nicolito128@hotmail.com>
2021-05-16 00:08:48 -0300

    Create messager.ts

---
+++ src/messager.ts
@@ -0,0 +1,54 @@
+import { Message, Guild, User } from 'discord.js'
+import { Arguments } from './lib/command'
+import { PluginSystem } from './plugins'
+import { App } from './client'
+import Config from './Config'
+
+/**
+	* MessageHandler:
+	* 	Eval messages and provide responses.
+*/
+export class MessageEvents {
+    private _plugins: PluginSystem;
+    private settedPlugins: boolean;
+
+    constructor() {
+        this._plugins = Object.create(null);
+        this.settedPlugins = false;
+    }
+
+    setPlugins(system: PluginSystem): void {
+        this._plugins = system;
+        this.settedPlugins = true;
+    }
+
+	eval(message: Message): void {
+        if (!this.settedPlugins) {
+            throw new Error("'plugins' must be configurated to work.")
+        }
+
+		if (message.author.bot) return;
+
+		if (
+			message.content
+			.toLowerCase()
+			.trim()
+			.substring(0, Config.prefix.length) === Config.prefix
+		) {
+			const params = this.getRunArguments(message)
+			this._plugins.emitCommand(params)
+		}
+	}
+
+	private getRunArguments(message: Message): Arguments {
+		const user: User = message.author;
+        const guild: Guild | null = (message.guild) ? message.guild : null;
+        const targets: string[] = message.content.slice(Config.prefix.length).trim().split(' ');
+        const cmd: string = targets?.shift()?.toLowerCase() || "";
+        const client = App;
+
+        return {message, client, user, guild, targets, cmd}
+	}
+}
+
+export const Messager = new MessageEvents();
